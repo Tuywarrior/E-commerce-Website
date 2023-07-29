25e7b4165323810d905aece73f270943c7b3abd5
Aaditya Rathi <87844490+Tuywarrior@users.noreply.github.com>
2023-07-29 22:38:09 +0530

    Add files via upload

---
+++ cart/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from store.models import Product
+from django.shortcuts import get_object_or_404
+
+from .cart import Cart
+from django.http import JsonResponse
+# Create your views here.
+
+
+def cart_summary(request):
+    cart = Cart(request)
+    return render(request, 'cart/cart-summary.html', {'cart': cart})
+
+
+def cart_add(request):
+
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product, product_qty=product_quantity)
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'qty': cart_quantity})
+        return response
+
+
+def cart_delete(request):
+
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+
+        cart.delete(product=product_id)
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+        response = JsonResponse({'qty': cart_quantity, 'total': cart_total})
+
+        return response
+
+
+def cart_update(request):
+
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        cart.update(product=product_id, qty=product_quantity)
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+        response = JsonResponse({'qty': cart_quantity, 'total': cart_total})
+
+        return response
